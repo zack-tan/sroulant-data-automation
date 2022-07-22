718d5c6f9ac788444ed49ff873996bb234a2fa80
Zack Tan <zhi.tan@mail.mcgill.ca>
2022-07-22 16:23:22 -0400

    Script to refresh client-level aggregates now considers new entries too (i.e. clients who started from the most recent month) and will add them to aggregate table.

--- airtable_aggregate_update.py
+++ airtable_aggregate_update.py
@@ -7,30 +7,57 @@ import pyairtable as pyat
 from pyairtable import Api, Base, Table
 from pyairtable import formulas as fm
 import pandas as pd
+from collections import defaultdict
 
 API_KEY = 'xx'
 BASE_ID = 'xx'
 
 if __name__ == '__main__':
-    # api = Api('key2RncEIzaX4d00l')
-    # api.all('base_id', 'table_name')
-    # base = Base('apikey', 'base_id')
-    # base.all('table_name')
 
     # Read in data to import
     df = pd.read_csv("latest_month_cleaned.csv")
 
     # Initialize Airtable API connector
+    table = Table(API_KEY, BASE_ID, 'Client_Count_Monthly')
     table_agg = Table(API_KEY, BASE_ID, 'Client_Aggregates')
 
-    # Retrieve all records from aggregation table
+
+    ### TALLY AGAINST CLIENT_COUNT_MONTHLY TO CHECK IF NEW RECORDS EXIST (i.e. new clients)
+
+    # Retrieve all entries from client_count_monthly and aggregation table
+    table_rows = table.all()
     aggregation_rows = table_agg.all()
 
-    # Store Airtable IDs of retrieved records
-    records = []
-    for r in aggregation_rows:
-        records.append(r.get('id'))
+    # Store month-by-month data into a defaultdict - {name : [corresponding airtable_record_IDs]} 
+    name_id_dict = defaultdict(list)
+    for t in table_rows:
+        client_name = t.get('fields').get('CLIENT NAME')
+        name_id_dict[client_name].append(t.get('id'))
 
+    # Get list of all names for both aggregate and month-by-month tables
+    name_list_monthly = list(name_id_dict.keys())
+    name_list_agg = [r.get('fields').get('Name') for r in aggregation_rows]
+
+    # Check that all names in month-by-month appears in aggregate table too. If not, write those records into the aggregates table
+    # Format of output defaultdict - { 'Name' : [Airtable Record IDs] }
+    name_id_missing = defaultdict(list)
+
+    for n in name_list_monthly:
+        if n not in name_list_agg:
+            name_id_missing[n] = name_id_dict.get(n)
+
+    # Add missing records into aggregate table
+    missing_names = list(name_id_missing.keys())
+
+    # for n in missing_names:
+    #     table_agg.create({ 'Name': n, 'Update': False })
+    [table_agg.create({ 'Name': n, 'Update': False}) for n in missing_names]
+    
+    
+    # Recollect the total number of rows in aggregated table and store Airtable IDs of retrieved records
+    aggregation_rows = table_agg.all()
+    records = [r.get('id') for r in aggregation_rows]
+    
     # Refresh all records - Works with an automation built within Airtable
     for i, r in enumerate(records):
         # Uncheck all Update boxes and clear lookup records
@@ -41,4 +68,4 @@ if __name__ == '__main__':
         
         print(f"Refreshed {i+1} out of {len(records)} entries.")
     
-    print("Operation completed")
+    print("Operation completed.")
